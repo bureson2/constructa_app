d3b853f3d52f4c19a80c4381bc538394ac0121be
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2023-04-17 10:55:15 +0200

    init commit

---
+++ constants/theme.js
@@ -0,0 +1,48 @@
+const COLORS = {
+    primaryFirst: "#302EAC",
+    primarySecond: "#0C54BC",
+    secondary: "#F7F8F6",
+
+    textColor: "#3C3939",
+
+    white: "#ffffff",
+    iconBackgroundColor: "#F7F7F6",
+};
+
+const FONT = {
+    regular: "Roboto",
+};
+
+const SIZES = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+};
+
+const SHADOWS = {
+    small: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 2,
+    },
+    medium: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 5.84,
+        elevation: 5,
+    },
+};
+
+export { COLORS, FONT, SIZES, SHADOWS };
